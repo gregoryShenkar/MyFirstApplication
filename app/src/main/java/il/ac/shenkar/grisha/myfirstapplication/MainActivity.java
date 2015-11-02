f4354e0a40b526d3f385b678c1cb0e84dd906eeb
kogrego <kogrego@gmail.com>
2015-11-02 15:26:37 +0200

    created first app .java and .xml

--- app/src/main/java/il/ac/shenkar/grisha/myfirstapplication/MainActivity.java
+++ app/src/main/java/il/ac/shenkar/grisha/myfirstapplication/MainActivity.java
@@ -2,12 +2,24 @@ package il.ac.shenkar.grisha.myfirstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "il.ac.shenkar.grisha.myfirstapplication.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
